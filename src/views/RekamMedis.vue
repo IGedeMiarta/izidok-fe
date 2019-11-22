7ce66a3f5d4493ac17baacb1af56e10932d4197c
Rezpa Aditya <rezpaditya@Rezpas-MacBook-Pro.local>
2019-11-22 22:54:10 +0700

    rekam medis [*one more step]

--- src/views/RekamMedis.vue
+++ src/views/RekamMedis.vue
@@ -5,7 +5,49 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <container/>
+                    <div>
+                      <div class="accordion mb-5">
+                          <div class="card card-box">
+                              <div class="card-header">
+                                  <b-button class="btn-link btn-lg d-flex align-items-center justify-content-between shadow-none" v-b-toggle.accordion-1>
+                                      <span>Anamnesa</span>
+                                      <font-awesome-icon icon="angle-up" class="font-size-xl"/>
+                                  </b-button>
+                              </div>
+                              <b-collapse id="accordion-1" visible accordion="accordion-example" role="tabpanel">
+                                  <div class="card-body">
+                                      <Anamnesa/>
+                                  </div>
+                              </b-collapse>
+                          </div>
+                          <div class="card card-box">
+                              <div class="card-header">
+                                  <b-button class="btn-link btn-lg d-flex align-items-center justify-content-between shadow-none collapsed" v-b-toggle.accordion-2>
+                                      <span>Pemeriksaan Fisik</span>
+                                      <font-awesome-icon icon="angle-up" class="font-size-xl"/>
+                                  </b-button>
+                              </div>
+                              <b-collapse id="accordion-2" accordion="accordion-example" role="tabpanel">
+                                <div class="card-body">
+                                  <Pemeriksaan/>   
+                                </div>
+                              </b-collapse>
+                          </div>
+                          <div class="card card-box">
+                              <div class="card-header">
+                                  <b-button class="btn-link btn-lg d-flex align-items-center justify-content-between shadow-none collapsed" v-b-toggle.accordion-3>
+                                      <span>Diagnosa</span>
+                                      <font-awesome-icon icon="angle-up" class="font-size-xl"/>
+                                  </b-button>
+                              </div>
+                              <b-collapse id="accordion-3" accordion="accordion-example" role="tabpanel">
+                                  <div class="card-body">
+                                    <Diagnosa/>
+                                  </div>
+                              </b-collapse>
+                          </div>
+                      </div>
+                  </div>
                 </div>
             </div>
         </div>
@@ -13,13 +55,17 @@
 </template>
 
 <script>
-import container from './RekamMedis/RekamMedisContainer.vue';
 import top from './RekamMedis/Header.vue';
+import Anamnesa from './RekamMedis/Anamnesa';
+import Pemeriksaan from './RekamMedis/Pemeriksaan.vue';
+import Diagnosa from './RekamMedis/Diagnosa';
 
 export default {
   components: {
     top,
-    container
+    Anamnesa,
+    Pemeriksaan,
+    Diagnosa
   }
 };
 </script>
---
+++ src/views/RekamMedis/Anamnesa.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <form>
+            <div class="form-row">
+                <div class="form-group col-md-2">
+                    <label for="tdSistole">TD Sistole*</label>
+                    <input type="text" class="form-control" id="tdSistole">
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="tdDiastole">TD Diastole*</label>
+                    <input type="text" class="form-control" id="tdDiastole">
+                </div>
+                <div class="form-group col-md-1">
+                    <label for="tinggiBadan">TB*</label>
+                    <input type="text" class="form-control" id="tinggiBadan">
+                </div>
+                <div class="form-group col-md-1">
+                    <label for="beratBadan">BB*</label>
+                    <input type="text" class="form-control" id="beratBadan">
+                </div>
+            </div>
+            <div class="form-row">
+               <div class="form-group col-md-2">
+                    <label for="nadi">Nadi*</label>
+                    <input type="text" class="form-control" id="nadi">
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="suhu">Suhu*</label>
+                    <input type="text" class="form-control" id="suhu">
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="respirasi">Respirasi*</label>
+                    <input type="text" class="form-control" id="respirasi">
+                </div>
+            </div>
+        </form>
+
+    </div>
+</template>
