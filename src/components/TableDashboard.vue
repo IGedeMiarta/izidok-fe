17e5f453966b2bf9d809192f74f5eba587c35c37
Muhammad Alfathan Harriz <harrizsb@gmail.com>
2020-01-28 16:34:18 +0700

    update

--- src/components/TableDashboard.vue
+++ src/components/TableDashboard.vue
@@ -21,7 +21,7 @@
               </tr>
             </thead>
             <tbody class="list">
-              <tr v-for="(data, index) in DataAnteranToday" :key="data.id">
+              <tr v-for="(data, index) in dataRawatJalan" :key="data.id">
                 <td class="text-wrap">
                   <div class="align-box-row">
                     <div class="d-flex align-items-center">
@@ -49,8 +49,12 @@
               </tr>
             </tbody>
           </table>
-          <b-pagination class="d-flex justify-content-center mt-4" v-model="currentPage" :total-rows="rows"
-            :per-page="perPage"></b-pagination>
+          <b-pagination
+            class="d-flex justify-content-center mt-4"
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="perPage"
+          ></b-pagination>
         </div>
         <div class="divider"></div>
       </div>
@@ -59,49 +63,63 @@
 </template>
 
 <script>
-  import {
-    library
-  } from "@fortawesome/fontawesome-svg-core";
-  import {
-    faSignInAlt
-  } from "@fortawesome/free-solid-svg-icons";
-  library.add(faSignInAlt);
-  import axios from "axios";
-  import moment from "moment";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+library.add(faSignInAlt);
+import axios from "axios";
+import moment from "moment";
 
-  export default {
-    data: () => ({
-      DataAnteranToday: [],
-      currentPage: 1,
-      perPage: 5,
-      rows: 0,
-    }),
-    mounted() {
-      this.getAntrian();
-    },
-    computed: {
-      now() {
-        return moment().format("YYYY-MM-DD");
-      }
-    },
-    methods: {
-      async getAntrian() {
-        try {
-          const res2 = await axios.get(
-            `${this.url_api}/transaksi?limit=10&status=MENUNGGU&from=${this.now}&to=${this.now}`);
-          const {
-            status,
-            data
-          } = res2.data;
-          
-          if (status) {
-            this.DataAnteranToday = res2.data.data.data;
+export default {
+  data: () => ({
+    dataRawatJalan: [],
+    currentPage: 1,
+    perPage: 5,
+    rows: 0
+  }),
+  mounted() {
+    this.getRawatJalanData();
+  },
+  computed: {
+    now() {
+      return moment().format("YYYY-MM-DD");
+    }
+  },
+  methods: {
+    async getRawatJalanData() {
+      try {
+        const res = await axios.get(
+          `${this.url_api}/transaksi?limit=10&status=MENUNGGU&from=${this.now}&to=${this.now}`
+        );
+        const {
+          status,
+          data: {
+            data: { data: rawatJalanData }
           }
-        } 
-        catch (err) {
-          console.log(err);
+        } = res;
+        if (status) {
+          this.dataRawatJalan = rawatJalanData;
         }
+      } catch (err) {
+        console.log(err);
       }
     }
-  };
+    // async getAntrian() {
+    //   try {
+    //     const res2 = await axios.get(
+    //       `${this.url_api}/transaksi?limit=10&status=MENUNGGU&from=${this.now}&to=${this.now}`);
+    //     const {
+    //       status,
+    //       data
+    //     } = res2.data;
+
+    //     if (status) {
+    //       this.DataAnteranToday = res2.data.data.data;
+    //     }
+    //   }
+    //   catch (err) {
+    //     console.log(err);
+    //   }
+    // }
+  }
+};
 </script>
