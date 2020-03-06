74f2d5b632daa39cb9058c5c01704499bc18ee64
Rian Nugraha <calledme.rian@gmail.com>
2020-03-07 04:06:05 +0700

    send struk email

--- src/views/Pembayaran/StrukWrapper.vue
+++ src/views/Pembayaran/StrukWrapper.vue
@@ -18,12 +18,12 @@
       <div class="card card-box mb-3 ninja-shadow">
         <div class="card-header">
           <div class="card-header--title">
-            <h3 class="text-capitalize">preview struk</h3>
+            <h3 class="text-capitalize">struk pembayaran</h3>
           </div>
         </div>
         <div class="card-body">
           <div class="d-flex flex-column">
-            <div class="d-flex flex-row">
+            <div class="d-flex flex-row justify-content-center">
               <div>
                 <button 
                   type="button" 
@@ -49,15 +49,45 @@
               </div>
             </div>
             <div class="d-flex flex-row">
-              <div class="d-flex flex-column justify-cotent-center">
+              <div class="d-flex flex-column justify-cotent-center w-100">
                 <!-- <Struk class="flex-grow-1" /> -->
-                <iframe ref="iframeStruk" src="" frameborder="0" height="600" width="362"></iframe>
+                <iframe ref="iframeStruk" src="" frameborder="0" :style="iframeStrukStyle"></iframe>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    
+    <b-modal id="modalEmailPasien" title="Masukkan Email Untuk Mengirimkan Struk" ok-only>
+      <div role="group">
+        <label for="emailPasien">Email <span class="text-danger">*</span></label>
+        <b-form-input 
+          id="emailPasien"
+          aria-describedby="emailPasien-help emailPasien-feedback"
+          :state="emailError == ''"
+          v-model="emailPasien"
+        ></b-form-input>
+
+        <b-form-invalid-feedback id="emailPasien-feedback" v-if="emailError != ''">
+          {{ emailError }}
+        </b-form-invalid-feedback>
+      </div>
+
+      <template v-slot:modal-footer>
+        <div class="w-100">
+          <b-button
+            variant="primary"
+            size="sm"
+            class="float-right ml-2"
+            @click="sendEmail"
+            :disabled="emailError != ''"
+          >
+            Kirim Email
+          </b-button>
+        </div>
+      </template>
+    </b-modal>
   </div>
 </template>
 
@@ -73,17 +103,49 @@ export default {
   data() {
     return {
       pembayaran_id: null,
-      afterPrintInjected: false
+      afterPrintInjected: false,
+      iframeStrukStyle: {
+        height: '600px',
+        width: '362px',
+        margin: '1em auto',
+        display: 'block'
+      },
+      emailPasien: null,
+      emailValidation: {
+        required: false,
+        format: false,
+      }
+    }
+  },
+  watch: {
+    emailPasien(newVal) {
+      this.emailValidation.required = (newVal != '');
+      this.emailValidation.format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(newVal);
     }
   },
   computed: {
     ...mapGetters({
       strukVal: "struk/struk_values"
-    })
+    }),
+    emailError() {
+      if(this.emailValidation.required == false) {
+        return "Email Harus Diisi"
+      }
+      else if(this.emailValidation.format == false) {
+        return "Format Email Tidak Sesuai"
+      }
+      else return "";
+    }
   },
   mounted() {
     this.pembayaran_id = this.$router.currentRoute.params.pembayaran_id;
     this.fetchStruk()
+    
+    this.$swal({
+      text: `Struk berhasil dikirimkan ke alamat email 'rian.nugraha@medlinx.co.id'`,
+      type: "success",
+      confirmButtonText: startCase("ok")
+    });
   },
   methods: {
     fetchStruk() {
@@ -128,7 +190,24 @@ export default {
       this.$refs.iframeStruk.contentWindow.print()
     },
     emailStruk() {
-      console.log('email')
+      axios.get(`${this.url_api}/pembayaran/pasien-email/${this.pembayaran_id}`)
+        .then(res => {
+          this.emailPasien = res.data;
+        })
+        .catch()
+        .finally(() => {
+          this.$bvModal.show('modalEmailPasien')
+        });
+    },
+    sendEmail() {
+      axios.get(`${this.url_api}/pembayaran/struk/${this.pembayaran_id}?email=${this.emailPasien}`)
+        .then(res => {
+          this.$swal({
+            title: `Struk berhasil dikirimkan ke alamat email '${this.emailPasien}'`,
+            type: "success",
+            confirmButtonText: startCase("ok")
+          });
+        });
     }
   }
 };
