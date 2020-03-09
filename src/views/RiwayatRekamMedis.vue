7aa03ebfc60a5b4536750fe48017ae656cac3da7
Dawai Pagi <abdie.dawaipagi@gmail.com>
2020-03-09 19:05:16 +0700

    fixing detail rekam medis

--- src/views/RiwayatRekamMedis.vue
+++ src/views/RiwayatRekamMedis.vue
@@ -105,7 +105,7 @@
       async fetchPasien() {
         let res = await axios.get(`${this.url_api}/pasien/${this.pasien_id}`);
         console.log(res.data.status);
-        if (res.data.status == false) {
+        if (res.data.status == false || res.data.data_rm == false) {
           this.$swal({
             type: "error",
             title: startCase("Gagal"),
